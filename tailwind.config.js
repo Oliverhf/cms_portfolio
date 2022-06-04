59a64eb8e6aed0a0bd3aba0c36ac7042d67d5e7f
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-06-04 02:25:45 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: '#F0EFF4',
+        gray: '#6D7275',
+        lightGray: '#C7D6D5',
+        black: '#201A23',
+        purple: '#832161',
+        pink: '#DA4167',
+        rasinBlack: '#2E2532'
+      },
+    },
+  },
+  plugins: [],
+}
