9309e951a4aa1a548f4dcc9f06d09e2e0c1e9f7b
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-06-07 23:04:37 -0300

    Add Footer for the Layout
    A footer for the webiste.

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-black h-80'>
+        <div> 
+
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
